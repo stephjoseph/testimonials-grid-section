a7ea60748ca376f2f24cb52412cbf1e8d4d5cd39
Stephen Joseph <80311745+stephjoseph@users.noreply.github.com>
2022-02-16 07:39:18 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import React from "react";
 import "./index.css";
+import Testimonials from "./components/Testimonials";
 
 function App() {
   return (
-    <div className="App mx-auto my-[4.438rem] grid h-[114rem] w-[20.438rem]  grid-cols-1 gap-6 "></div>
+    <div className="App mx-auto my-[4.438rem] h-[114rem] w-[20.438rem]">
+      <Testimonials />
+    </div>
   );
 }
 
