79d2091867980f5c52c656e91b9d1705c44b4887
Stephen Joseph <80311745+stephjoseph@users.noreply.github.com>
2022-02-16 09:46:34 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Testimonials from "./components/Testimonials";
 
 function App() {
   return (
-    <div className="App mx-auto my-[4.438rem] h-[114rem] w-[20.438rem] font-['Barlow_Semi_Condensed']">
+    <div className="App mx-auto my-[4.438rem] h-[114rem] w-[20.438rem] font-['Barlow_Semi_Condensed'] xl:my-[10.25rem] xl:h-[35.75rem] xl:w-[69.375rem]">
       <Testimonials />
     </div>
   );
